60308f75441fc27c2ac0889d3240293f6489deb9
André Vinícius Coelho da Silva <37165406+andrevinni@users.noreply.github.com>
2021-12-29 22:06:44 -0300

    Add files via upload

---
+++ Python/Flask/Hello_World/application.py
@@ -0,0 +1,23 @@
+# import the Flask class from the flask module
+from flask import Flask, render_template
+
+# create the application object
+app = Flask(__name__)
+
+# use decorators to link the function to a url
+@app.route('/')
+def home():
+    return "Hello, World!"  # return a string
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')  # render a template
+
+@app.route("/teste")
+def index():
+  return render_template('index.html')
+
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
+
